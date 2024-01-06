f01a8c6ea05b6ae6c3603d10ce5ebd39eba3c680
Mvahabi <mahyarvahabi@gmail.com>
2024-01-06 06:51:49 -0800

    done?!?

--- assets/php/send-email.php
+++ assets/php/send-email.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   mail($to, $subject, $message, $headers);
 }
 ?> -->
-<?php
+<!-- "<"?php
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -25,7 +25,7 @@ $successMessage = '';
 $siteKey = ''; // reCAPTCHA site key
 $secret = ''; // reCAPTCHA secret key
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = sanitizeInput($_POST['name']);
     $email = sanitizeInput($_POST['email']);
     $message = sanitizeInput($_POST['message']);
@@ -92,4 +92,24 @@ function sanitizeInput($input) {
    return $input;
 }
 
+?> -->
+
+<?php 
+if(isset($_POST['submit'])){
+    $to = "mahyarvahabi@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
